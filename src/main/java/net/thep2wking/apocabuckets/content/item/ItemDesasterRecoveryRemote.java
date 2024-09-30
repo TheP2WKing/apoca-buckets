d6c88f58be83b68e0741b278f381ad79f9252131
TheP2WKing <tdziomba@hotmail.com>
2024-09-30 18:24:54 +0200

    updated chat component to use the new system

--- src/main/java/net/thep2wking/apocabuckets/content/item/ItemDesasterRecoveryRemote.java
+++ src/main/java/net/thep2wking/apocabuckets/content/item/ItemDesasterRecoveryRemote.java
@@ -8,12 +8,14 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.thep2wking.apocabuckets.config.ApocaBucketsConfig;
 import net.thep2wking.apocabuckets.util.handler.ModWorldSavedData;
 import net.thep2wking.apocabuckets.util.network.MessageStopApocalypse;
 import net.thep2wking.apocabuckets.util.network.ModNetworkHandler;
 import net.thep2wking.oedldoedlcore.api.item.ModItemBase;
+import net.thep2wking.oedldoedlcore.util.ModTooltips;
 
 public class ItemDesasterRecoveryRemote extends ModItemBase {
     public ItemDesasterRecoveryRemote(String modid, String name, CreativeTabs tab, EnumRarity rarity, boolean hasEffect,
@@ -24,6 +26,7 @@ public class ItemDesasterRecoveryRemote extends ModItemBase {
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+        ItemStack stack = player.getHeldItem(hand);
         if (ApocaBucketsConfig.CONTENT.DESASTER_RECOVERY_REMOTE_CONTROLS_APOCALYPSES) {
             if (!world.isRemote) {
                 ModNetworkHandler.NETWORK.sendToServer(new MessageStopApocalypse(true));
@@ -32,19 +35,19 @@ public class ItemDesasterRecoveryRemote extends ModItemBase {
                 if (ApocaBucketsConfig.CONTENT.DESASTER_RECOVERY_REMOTE_USEABLE_IN_SURVIVAL
                         || player.capabilities.isCreativeMode) {
                     if (!isStopped) {
-                        player.sendMessage(new TextComponentTranslation(this.getUnlocalizedName() + ".tip1"));
+                        ModTooltips.sendItemInfoChatComponent(player, stack, 1, TextFormatting.LIGHT_PURPLE);
                     } else {
-                        player.sendMessage(new TextComponentTranslation(this.getUnlocalizedName() + ".tip2"));
+                        ModTooltips.sendItemInfoChatComponent(player, stack, 2, TextFormatting.LIGHT_PURPLE);
                     }
                 } else {
-                    player.sendMessage(new TextComponentTranslation(this.getUnlocalizedName() + ".tip3"));
+                    ModTooltips.sendItemInfoChatComponent(player, stack, 3, TextFormatting.LIGHT_PURPLE);
                 }
             }
             player.swingArm(hand);
             return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
         }
         if (!world.isRemote) {
-            player.sendMessage(new TextComponentTranslation(this.getUnlocalizedName() + ".tip3"));
+            ModTooltips.sendItemInfoChatComponent(player, stack, 3, TextFormatting.LIGHT_PURPLE);
         }
         player.swingArm(hand);
         return new ActionResult<>(EnumActionResult.FAIL, player.getHeldItem(hand));
