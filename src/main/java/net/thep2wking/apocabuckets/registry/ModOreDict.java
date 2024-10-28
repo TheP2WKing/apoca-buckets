aed7db3bb7b4d7966171c014c135f782b6c63445
TheP2WKing <tdziomba@hotmail.com>
2024-10-28 21:02:01 +0100

    fire oredict

--- src/main/java/net/thep2wking/apocabuckets/registry/ModOreDict.java
+++ src/main/java/net/thep2wking/apocabuckets/registry/ModOreDict.java
@@ -3,11 +3,15 @@ package net.thep2wking.apocabuckets.registry;
 import net.thep2wking.oedldoedlcore.util.ModLogger;
 import net.thep2wking.oedldoedlcore.util.ModRecipeHelper;
 import net.thep2wking.apocabuckets.ApocaBuckets;
+import net.thep2wking.apocabuckets.init.ModBlocks;
 import net.thep2wking.apocabuckets.init.ModItems;
 
 public class ModOreDict {
 	public static void registerOreDict() {
 		ModLogger.registeredOreDictLogger(ApocaBuckets.MODID);
+
+		ModRecipeHelper.addOreDict("fire", ModItems.FIRE, 0);
+		ModRecipeHelper.addOreDict("fireGreen", ModBlocks.GREEN_FIRE, 0);
 
 		ModRecipeHelper.addOreDict("bucketApocalypticEmpty", ModItems.APOCALYPTIC_BUCKET, 0);
 
